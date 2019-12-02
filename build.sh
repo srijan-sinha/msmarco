cbf0e9625f2cc29308dcf1f317e85739741e66c6
srijan-sinha <srijansinha159@gmail.com>
2019-12-03 03:16:24 +0530

    Updated scripts

--- build.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p misc
-mkdir -p output
-mkdir -p test_data
-mkdir -p train_data
-python data_gen.py -q /scratch/cse/phd/csz188014/IR_assig2_data/data-release/qrels.train.tsv -t /scratch/cse/phd/csz188014/IR_assig2_data/data_release/top1000.train.txt -d ./train_data/small_data.csv -i ./train_data/small_id.csv -a ./train_data/large_data.csv -b ./train_data/large_id.csv
-python train.py -d ./data/large_data.csv -i ./data/large_id.csv -m ./misc/model -f blank -b ./misc/input_id_train.csv
--- rerank.sh
+++ rerank.sh
@@ -1,21 +1,26 @@
-# $2 query file
-# $4 output file
-# $6 top1000.eval.txt
-python data_test.py -e /scratch/cse/phd/csz188014/IR_assig2_data/data_release/top1000.eval.txt -t ./test_data/
+#!/bin/bash
 
-python eval_single ./test_data/ 0 -e -i ./misc/ -r ./misc -m ./misc/model1.pkl -m model -o ./trec
-python eval_single ./test_data/ 1 -e -i ./misc/ -r ./misc -m ./misc/model1.pkl -m model -o ./trec
-python eval_single ./test_data/ 2 -e -i ./misc/ -r ./misc -m ./misc/model1.pkl -m model -o ./trec
-python eval_single ./test_data/ 3 -e -i ./misc/ -r ./misc -m ./misc/model1.pkl -m model -o ./trec
-python eval_single ./test_data/ 4 -e -i ./misc/ -r ./misc -m ./misc/model1.pkl -m model -o ./trec
-python eval_single ./test_data/ 5 -e -i ./misc/ -r ./misc -m ./misc/model1.pkl -m model -o ./trec
-python eval_single ./test_data/ 6 -e -i ./misc/ -r ./misc -m ./misc/model1.pkl -m model -o ./trec
+# $1 output file
+
+mkdir -p ./trec
+
+python ./code/data_test.py -e /scratch/cse/phd/csz188014/IR_assig2_data/data_release/top1000.eval.txt -t ./test_data/
+
+# Execute these 7 commands parallely on different hoc jobs
+
+python ./code/eval_single.py -t ./test_data/ -p 0 -e -i ./misc/ -r ./misc -m ./misc/model.pkl -n model -o ./trec
+python ./code/eval_single.py -t ./test_data/ -p 1 -e -i ./misc/ -r ./misc -m ./misc/model.pkl -n model -o ./trec
+python ./code/eval_single.py -t ./test_data/ -p 2 -e -i ./misc/ -r ./misc -m ./misc/model.pkl -n model -o ./trec
+python ./code/eval_single.py -t ./test_data/ -p 3 -e -i ./misc/ -r ./misc -m ./misc/model.pkl -n model -o ./trec
+python ./code/eval_single.py -t ./test_data/ -p 4 -e -i ./misc/ -r ./misc -m ./misc/model.pkl -n model -o ./trec
+python ./code/eval_single.py -t ./test_data/ -p 5 -e -i ./misc/ -r ./misc -m ./misc/model.pkl -n model -o ./trec
+python ./code/eval_single.py -t ./test_data/ -p 6 -e -i ./misc/ -r ./misc -m ./misc/model.pkl -n model -o ./trec
 
 touch $4
-cat ./trec/trecinput_model_0.txt >> $4
-cat ./trec/trecinput_model_1.txt >> $4
-cat ./trec/trecinput_model_2.txt >> $4
-cat ./trec/trecinput_model_3.txt >> $4
-cat ./trec/trecinput_model_4.txt >> $4
-cat ./trec/trecinput_model_5.txt >> $4
-cat ./trec/trecinput_model_6.txt >> $4
+cat ./trec/trecinput_model_0.txt >> $1
+cat ./trec/trecinput_model_1.txt >> $1
+cat ./trec/trecinput_model_2.txt >> $1
+cat ./trec/trecinput_model_3.txt >> $1
+cat ./trec/trecinput_model_4.txt >> $1
+cat ./trec/trecinput_model_5.txt >> $1
+cat ./trec/trecinput_model_6.txt >> $1
